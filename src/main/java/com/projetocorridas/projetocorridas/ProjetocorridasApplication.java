d07588670f9efb6987a0e0f77c6c7d8ba49cccba
Andersonlima13 <anderson.lima18pb@gmail.com>
2026-05-12 21:34:31 -0300

    first commit

---
+++ src/main/java/com/projetocorridas/projetocorridas/ProjetocorridasApplication.java
@@ -0,0 +1,13 @@
+package com.projetocorridas.projetocorridas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetocorridasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetocorridasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetocorridas/projetocorridas/ProjetocorridasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetocorridas.projetocorridas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetocorridasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
