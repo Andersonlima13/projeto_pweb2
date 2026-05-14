551a2837614c467662f5a3b920f648a76bdd3f15
Andersonlima13 <anderson.lima18pb@gmail.com>
2026-05-13 21:49:12 -0300

    criando demais dtos

--- src/main/java/com/projetocorridas/projetocorridas/dto/AlternativaDto.java
+++ src/main/java/com/projetocorridas/projetocorridas/dto/AlternativaDto.java
@@ -1,5 +1,20 @@
 package com.projetocorridas.projetocorridas.dto;
 
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AlternativaDto {
+    private UUID id;
+    private long corridaId;
+    private long perguntaId;
+    private String descricao;
+    private Boolean isCorreta;
 
 }
--- src/main/java/com/projetocorridas/projetocorridas/dto/PerguntaDto.java
+++ src/main/java/com/projetocorridas/projetocorridas/dto/PerguntaDto.java
@@ -1,5 +1,22 @@
 package com.projetocorridas.projetocorridas.dto;
 
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PerguntaDto {
+    private UUID id;
+    private long corridaId;
+    private String enunciado;
+    private long respostaCorreta;
+    private List<AlternativaDto> alternativas;
 
 }
