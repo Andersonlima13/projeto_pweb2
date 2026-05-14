240fc2a0931acf17d7b9df94e0f1755e6d060dae
Andersonlima13 <anderson.lima18pb@gmail.com>
2026-05-13 21:59:44 -0300

    bug de pacote , corrida repository resolvido

--- src/main/java/com/projetocorridas/projetocorridas/repository/CorridaRepository.java
+++ src/main/java/com/projetocorridas/projetocorridas/repository/CorridaRepository.java
@@ -1,3 +1,5 @@
+package com.projetocorridas.projetocorridas.repository;
+
 import com.projetocorridas.projetocorridas.model.Corrida;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
