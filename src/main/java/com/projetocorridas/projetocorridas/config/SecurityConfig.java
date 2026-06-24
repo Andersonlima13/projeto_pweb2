238f6d2358cb9fc77c2ec8ad24d0eb124bd29e6d
andersonlima13 <anderson.lima18pb@gmail.com>
2026-06-23 23:38:29 -0300

    config do spring security - exigindo autenticacao de usuarios

--- src/main/java/com/projetocorridas/projetocorridas/config/SecurityConfig.java
+++ src/main/java/com/projetocorridas/projetocorridas/config/SecurityConfig.java
@@ -3,21 +3,31 @@ package com.projetocorridas.projetocorridas.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll())
-                .formLogin(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable);
+                        // Login é público
+                        .requestMatchers("/auth/login").permitAll()
+                        .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
+                        // Todas as outras rotas exigem autenticação
+                        .anyRequest().authenticated())
+                .formLogin(form -> form.disable())
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .csrf(csrf -> csrf.disable());
 
         return http.build();
     }
